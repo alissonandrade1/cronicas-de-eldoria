3bebd40726f1b03552ba7dffe3a5409d5a96c0d5
alissonandrade1 <alissonandrade0211@gmail.com>
2025-10-31 12:50:47 -0300

    chore: inicializar projeto Spring Boot com dependenêas  mínimass

---
+++ src/main/java/com/alissondev/cronicas_de_eldoria/CronicasDeEldoriaApplication.java
@@ -0,0 +1,13 @@
+package com.alissondev.cronicas_de_eldoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronicasDeEldoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronicasDeEldoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alissondev/cronicas_de_eldoria/CronicasDeEldoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissondev.cronicas_de_eldoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronicasDeEldoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
